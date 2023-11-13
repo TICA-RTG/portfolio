a26124d013b7d6c06f2580971f5db3b0f17d8481
RTG <65651298+TICA-RTG@users.noreply.github.com>
2023-11-13 22:33:52 +0000

    minor change in about

--- portfolio/src/component/About.jsx
+++ portfolio/src/component/About.jsx
@@ -8,13 +8,13 @@ const About = ()=> {
                 <div className="bio">
                 <p>
                 A dedicated and versatile professional with a Bachelor's degree in Computer Science, 
-                earned in 2020, complemented by 1.5 years of hands-on experience as a data specialist. 
+                 complemented by 1.5 years of hands-on experience as a data specialist. 
                 Specialized in data annotation and gained expertise in computer vision and natural 
                 language processing. Proven ability to adapt and learn quickly, demonstrated through 
-                self-initiated pursuits, such as transitioning from piano playing to music production 
-                and expanding skills from software use to software development during free time. 
+                self-initiated pursuits, such as transitioning from music production 
+                to expanding skills from software use to software development during free time. 
                 Committed to continuous learning and adept at navigating diverse domains to broaden 
-                knowledge and skill sets. Passionate about pushing boundaries and contributing to 
+                knowledge and skill sets. I'm also very Passionate about pushing boundaries and contributing to 
                 the dynamic landscape of technology.
                 </p>
                 </div>
